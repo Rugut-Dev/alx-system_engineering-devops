5d2d80221c3719a29ed7b8c345ebaabe85cbe29d
Kelvin Rugut <krugut36@gmail.com>
2024-02-23 23:43:34 +0300

    Creating a file

--- 0X0A-configuration_management/0-create_a_file.pp
+++ 0X0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file at /tmp
 file { '/tmp/school':
-  ensure  => file,
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => "I love Puppet\n",
+  content => "I love Puppet",
 }
